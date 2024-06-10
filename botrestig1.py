f349aeef94bda758291cedbb0f26628b18250ac1
oshequene <usausukfrit@gmail.com>
2024-06-10 14:38:26 +0300

    Update botrestig1.py

--- botrestig1.py
+++ botrestig1.py
@@ -1,67 +1,55 @@
 import requests
-import telebot,time
-from gdolib import *
+import telebot
 from telebot import types
-url = 'https://i.instagram.com/api/v1/accounts/send_password_reset/'
 
 
+Tok = "6956462730:AAGPjYs3aW4nzqlatNLpwNWhyrA79ZSxZQ0"
+L7N = telebot.TeleBot(Tok)
 
-channel = types.InlineKeyboardButton(text='المطور', url = 't.me/exxxix')
-
-head = {
-    'authority': 'www.instagram.com',
-    'accept': '*/*',
-    'accept-language': 'ar',
-    'content-type': 'application/x-www-form-urlencoded',
-    'cookie': 'csrftoken=vEG96oJnlEsyUWNS53bHLkVTMFYQKCBV; ig_did=5D80D38A-797B-482D-A407-4B51217E09E7; ig_nrcb=1; mid=ZEqtPgALAAE-IVt6zG-ZazKzI4qN; datr=jrJKZGOaV4gHwa-Znj2QCVyB',
-    'origin': 'https://www.instagram.com',
-    'referer': 'https://www.instagram.com/accounts/password/reset/?next=%2Faccounts%2Flogout%2F',
-    'sec-ch-prefers-color-scheme': 'light',
-    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-    'viewport-width': '1261',
-    'x-asbd-id': '198387',
-    'x-csrftoken': 'vEG96oJnlEsyUWNS53bHLkVTMFYQKCBV',
-    'x-ig-app-id': '936619743392459',
-    'x-ig-www-claim': '0',
-    'x-instagram-ajax': '1007389883',
-    'x-requested-with': 'XMLHttpRequest',
+@L7N.message_handler(commands=['start'])
+def start(message):
+            	L7N1 = types.InlineKeyboardMarkup()
+            	L7Ntele = types.InlineKeyboardButton('Dev',url ='https://t.me/exxxix')
+            	L7N1.add(L7Ntele)
+            	photo = f"t.me/{message.from_user.username}"
+            	L7Ncall = f"[{message.from_user.first_name}]({photo})"
+            	text = f"- Hello {L7Ncall} ! To Bot Rest instagram "
+            	L7N.send_photo(message.chat.id,photo,text,parse_mode='Markdown',reply_markup=L7N1)
+            	L7N.reply_to(message,'<strong>-Send Me The User instagram :</strong> ',parse_mode='html')
+            	@L7N.message_handler(func=lambda m: True)
+            	def L7N_send(message):
+            				L7Nmes = message.text
+            				url = 'https://i.instagram.com/api/v1/accounts/send_password_reset/'
+            				head ={
+'Host': 'i.instagram.com',
+'Connection': 'Keep-Alive',
+'Cookie': 'mid=YgzPXAABAAFpu2FvBU3Nz814ooE5; csrftoken=DVpBRlGfuAZ0E7JtTnLD71F0mcnNV2tW',
+'Cookie2': '$Version=1',
+'Accept-Language': 'en-GB, en-US',
+'X-IG-Connection-Type': 'WIFI',
+'X-IG-Capabilities': 'AQ==',
+'Accept-Encoding': 'gzip'
 }
+            				data ={
+"user_email":f"{L7Nmes}",
+"device_id":"android-ae9d37a73aa41d00",
+"guid":"d038a34e-1663-4f2b-af9d-aae995d105c4",
+"_csrftoken":"DVpBRlGfuAZ0E7JtTnLD71F0mcnNV2tW"
+}
+            				req =requests.post(url,headers = head, data = data)
+            				if '"status":"ok"' in req.text:
+            					rL7N = req.json()['obfuscated_email']
+            					L7N.send_message(message.chat.id,f'''
+<strong>- Doon Sned Rest ↝ @{L7Nmes}
+————————×————————
+- Email Sent ↝</strong> {rL7N}
+<strong>————————×————————
+- By ↝ @exxxix </strong>
+            			''',parse_mode='html')
+            				else:
+            					L7N.send_message(message.chat.id,'*• اليوزر غير صحيح او تم ارسال ريست له من قبل.!*',parse_mode="Markdown")
 
-token = "6956462730:AAGPjYs3aW4nzqlatNLpwNWhyrA79ZSxZQ0"
-bot = telebot.TeleBot(token)
+# What are you doing? I see you steal  
+# ترجم كلمة steal
 
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bb = types.InlineKeyboardMarkup()
-    bb.add(channel)
-    bot.reply_to(message,"ارسل ايميلك لا ترسل اليوزر\n مثال :\nexample@gmail.com",reply_markup=bb)
-
-	
-@bot.message_handler(content_types=['text'])
-def rest(message):
-  email = message.text.strip()
-  
-  data ={
-      "user_email": email
-  }
-
-  try:
-  	req =requests.post(url,headers=head,data = data).json()
-  	st = req['status']
-  	if st == 'ok':
-  		r = req["obfuscated_email"]
-  		bot.reply_to(message,f'تم توصيل الريست:\n{r}')
-  	else:
-  		bot.reply_to(message,"Sorry, we can't send you a link to reset your password. Please contact Instagram for help.")
-  except:
-  	bot.reply_to(message,'Error')
- 
-
-bot.infinity_polling()
+L7N.infinity_polling()           			
